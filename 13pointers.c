8c2af729719c56bf28c24532cfea8c07ca2390c6
Anku-0101 <anku.kumar@strandls.com>
2020-11-22 18:13:38 +0530

    Commit_2

--- 13pointers.c
+++ 13pointers.c
@@ -14,6 +14,6 @@ int main()
     printf("*(&i) = %d\n", *(&i));
     printf("&(*ptr) = %d\n", &(*ptr));
     printf("*(&ptr) = %d\n", *(&ptr));
-   // printf("*i = %d\n", *i);
+   // printf("*i = %d\n", *i); // SYNTAX ERROR, Can’t use unary operator with one value
     return -1;
 }
