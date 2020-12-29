1aab504ae9e7c64422697592e1d0ee75350919e7
Anku-0101 <anku.kumar@strandls.com>
2020-12-29 17:32:09 +0530

    keeping Uptodate

---
+++ DynamicMemory/01Dynamic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*
+Dynamic memory allocation: 
+Dynamic memory allocation is a way to allocate memory to a data structure during runtime. 
+DMA functions available in C is used to allocate and free up memory during runtime.
+
+Functions for DMA in C
+1. malloc()
+2. calloc()
+3. free()
+4. realloc()
+*/
+
+
+int main()
+{
+    return 0;
+}
