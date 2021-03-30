4e241392b4eb9e1208f15e4a806204f7f4256489
Anku Choudhary <ankuchoudhary@Ankus-MacBook-Air.local>
2021-03-30 10:34:44 +0530

    Problems Added

--- CodeForces/CandiesAndTwoSisters1335A.c
+++ CodeForces/CandiesAndTwoSisters1335A.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
+/*
+n'+r-1Cr-1
+n' = n-2 [can't be 0]
+n'+1C1 = n'+1 = n-2+1 = n-1
+ANS = n-1/2
 
+*/
 int main()
 {
+    int t,n;
+    scanf("%d",&t);
+    
+    while (t--)
+    {
+        scanf("%d",&n);
+        if(n<3)
+        {
+            if(t!=0)printf("0\n");
+            else printf("0");
+        }
+        else
+        {
+            if(t!=0)printf("%d\n",(n-1)/2);
+            else printf("%d",(n-1)/2);
+        }
+    }
     
     return 0;
 }
--- CodeForces/Pangram520A.c
+++ CodeForces/Pangram520A.c
@@ -2,6 +2,29 @@
 
 int main()
 {
-    
+   int n;
+   scanf("%d",&n);
+   char str[n+1];
+   scanf("%s",str);
+   int arr[100];
+   for (int i = 0; i < 100; i++)arr[i] = 0;
+   for(int i =0; i<n; i++)
+   {
+       if(str[i]>90)str[i] -=32;
+       arr[str[i]]++;
+   }
+   int flag = 0;
+   for(int i =65; i<=90; i++)
+   {
+       if(arr[i] ==0)
+       {
+           flag = 1;
+           printf("NO");
+           break;
+       }
+   }
+   if(flag == 0)printf("YES");
+   
+
     return 0;
 }
