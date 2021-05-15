50824c8d2ce06cd59f84c2598428dd2b6620bae2
Anku Kumar <ankukumariitb@outlook.com>
2021-05-15 10:35:07 +0530

    Updated on 15th May 2021

--- CodeForces/Dragons230A.c
+++ CodeForces/Dragons230A.c
@@ -1,7 +1,92 @@
 #include<stdio.h>
+void BubbleSort(int arr[][2], int n)
+{
+    for(int i =0; i<n-1; i++)
+    {
+        for(int j = 0; j<n-i-1; j++)
+        {
+            if(arr[j][0] > arr[j+1][0])
+            {
+                int temp1 = arr[j+1][0];
+                int temp2 = arr[j+1][1];
+
+                arr[j+1][0] = arr[j][0];
+                arr[j][0] = temp1;
+
+                arr[j+1][1] = arr[j][1];
+                arr[j][1]  = temp2;
+            }
+        }
+    }
+}
 
 int main()
 {
+    int s;
+    scanf("%d",&s);
+    int n;
+    scanf("%d",&n);
     
+    int x[n];
+    int y[n];
+    int data[n][2];
+    
+    for(int i =0; i<n; i++)
+    {
+        scanf("%d",&data[i][0]);
+        scanf("%d",&data[i][1]);
+        //data[i][2] = data[i][1] - data[i][0];
+    }
+    
+    BubbleSort(data, n);
+    
+    /*
+    for(int i =0; i<n; i++)
+    {
+        printf(" x = %d  ",data[i][0]);
+        printf(" y = %d ",data[i][1]);
+        //printf(" diff = %d \n", data[i][2]);
+    }
+    */
+
+    int flag = 0;
+    for(int i =0; i<n; i++)
+    {
+        if(data[i][0] >= s)
+        {
+            flag = 1;
+            printf("NO");
+            break;
+        }
+        else
+        {
+            s += data[i][1];
+        }
+    }
+    if(flag == 0)printf("YES");
     return 0;
 }
+
+/*
+
+#include<stdio.h>
+int main()
+{
+	int n,s,x[1000],y[1000],i,r=0;
+	scanf("%d %d",&s,&n);
+	for(i=0;i<n;i++)scanf("%d %d",&x[i],&y[i]);
+	for(i=0;i<n;i++)
+	{
+		if(s>x[i]&&x[i]!=0)
+		{
+			s+=y[i];
+			x[i]=0;
+			i=-1;
+			r+=1;
+		}
+	
+	}
+	if (r==n) printf("YES");
+	else printf("NO");
+}
+*/
--- CodeForces/XeniaAndRingroad339B.c
+++ CodeForces/XeniaAndRingroad339B.c
@@ -2,6 +2,30 @@
 
 int main()
 {
+    int n, m;
+    scanf("%d%d",&n,&m);
+    int arr[m];
+    int x = m, c = 0;
     
+    while(c < m)  scanf("%d", &arr[c++]);    
+    
+    int curr = 1; 
+    long long steps = 0;
+    
+    for(int i = 0; i<m; i++)
+    {
+        if(arr[i] >= curr)
+        {
+            steps += arr[i] - curr;
+            curr = arr[i];
+        }
+        else
+        {
+            steps += n - curr + 1;
+            steps += arr[i] - 1;
+            curr = arr[i];
+        }
+    }
+    printf("%lld", steps);
     return 0;
 }
