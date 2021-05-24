53a686ed82585050dad761cc88c3dd520f2abc02
Anku Kumar <ankukumariitb@outlook.com>
2021-05-24 10:05:32 +0530

    problems solved, count of elements doing binary search updated

---
+++ DSA/Algorithm/BinarySearch/CountOfElement.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int CountOfElementInSortedArray(int arr[], int n, int x)
+{
+    int start = 0;
+    int end = n-1;
+    int count = 0;
+
+    while(start <= end)
+    {
+        int mid = start + (end - start)/2;
+        if(arr[mid] == x)
+        {
+            count++;
+            int index = mid;
+
+            while(--index >-1 && arr[index] == x) count++;
+            index = mid;
+            while(++index <n && arr[index] == x) count++;
+            
+            return count;
+        }
+        else if(arr[mid] > x) end = mid - 1;
+        else start = mid + 1;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int arr[] = {2,2,2,3,3,3,3,3,5,5,5,6,6,7,7,8,8,8,8,8,11,11,11};
+    printf("Enter the number to search \n"); 
+    int x;
+    scanf("%d", &x);
+    int result = CountOfElementInSortedArray(arr,23,x);
+    printf("The number %d is present in array %d number of time(s)\n", x,result);
+    return 0;
+}
