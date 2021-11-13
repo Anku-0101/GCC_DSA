adbf355c44ef0487500706ffd69e8ad46110181c
Anku Kumar <ankukumariitb@outlook.com>
2021-11-13 11:15:19 +0530

    Sorting algo added

---
+++ DSA/Algorithm/SortingAlgorithms/SelectionSort.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<limits.h>
+
+void SelectionSort(int arr[], int num)
+{
+    int sorted[num];
+    
+
+    for(int i = 0; i < num; i++)
+    {
+        int min = INT_MAX;
+        int k;
+        for(int j = i; j<num; j++)
+        {
+            if(arr[j] <= min)
+            {
+                min = arr[j];
+                k = j;
+            }                
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+}
+
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements present in the array \n");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i <n; i++)
+    {
+        printf("Enter the element at index  = %d ", i);
+        scanf("%d", &arr[i]);
+    }
+
+
+    SelectionSort(arr,n);
+
+    for(int i = 0; i < n; i++)
+        printf("arr[%d] = %d \n", i, arr[i]);
+
+}
