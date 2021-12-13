e0cb09fa976721e0c3092dd8c5f9c0b937b61cdf
Anku Kumar <ankukumariitb@outlook.com>
2021-12-13 12:54:44 +0530

    Problem solved

--- CodeForces/PoliceRecruits427A.c
+++ CodeForces/PoliceRecruits427A.c
@@ -2,6 +2,34 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
     
+    int ans = 0;
+    int sum = 0;
+
+    while(n--)
+    {
+        int input;  
+        scanf("%d", &input);
+        
+        if(input == -1)
+        {
+            sum--;
+
+            if(sum < 0)
+            {
+                sum = 0;
+                ans++;
+            }
+        }
+        else
+        {
+            sum += input;
+        }
+
+    }
+    
+    printf("%d", ans);
     return 0;
 }
