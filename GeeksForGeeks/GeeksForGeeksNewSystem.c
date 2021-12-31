358406e62f72ec419782514c2a066f9635acfa8d
Anku-0101 <anku.kumar@strandls.com>
2021-12-31 11:47:03 +0530

    GFG problem added

--- GeeksForGeeks/GeeksForGeeksNewSystem.c
+++ GeeksForGeeks/GeeksForGeeksNewSystem.c
@@ -1,8 +1,33 @@
 #include<stdio.h>
 
+int StringComparator(char str1[], char str2[])
+{
+
+	int l1 = StringLength(str1);
+	int l2 = StringLength(str2);
+
+	if(l1 != l2)
+		return 0;
+
+	for(int i = 0; i<l1; i++)
+		if(str1[i] != str2[i])
+			return 0;
+
+	return 1;
+}
+
+int StringLength(char str[])
+{
+	int length = 0;
+	for(int i = 0 ; str[i] != '\0'; i++)
+		length++;
+
+	return length;
+}
+
 int main()
 {
-	int n;
+	int N;
 	return 0;
 }
 
