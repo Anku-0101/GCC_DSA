1f7aea7bd4dd5d848fa6722e605e62b5acc35913
Anku Kumar <ankukumariitb@outlook.com>
2022-01-13 12:13:00 +0530

    two new problems submitted

--- CodeForces/RestoringThreeNumbers1154A.c
+++ CodeForces/RestoringThreeNumbers1154A.c
@@ -2,6 +2,28 @@
 
 int main()
 {
+    int arr[4];
+    int max = -1;
+
+    for(int i = 0; i<4; i++)
+    {
+        int num;  
+        scanf("%d", &num);
+        arr[i] = num;
+        
+        if(num > max)
+        {
+            max = num;
+        }
+    }
+
+    for(int i = 0; i<4; i++)
+    {
+        if(max - arr[i] != 0)
+            printf("%d ", max - arr[i]);
+    }
+    
+
     
     return 0;
 }
--- CodeForces/YetAnotherTwoIntegersProblem1409A.c
+++ CodeForces/YetAnotherTwoIntegersProblem1409A.c
@@ -2,6 +2,24 @@
 
 int main()
 {
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        
+        int diff = a - b;
+        
+        if(diff < 0)
+            diff *= -1;
+        
+        if(diff%10 == 0)
+            printf("%d\n",diff/10);
+        else
+            printf("%d\n", diff/10 +1);
+    }
     
     return 0;
 }
