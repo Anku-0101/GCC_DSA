31c5d80a850df9971a2d6bd7a44a49b1c687fd07
Anku Kumar <ankukumariitb@outlook.com>
2022-01-16 18:49:46 +0530

    two problems solved

--- CodeForces/BuyAShovel732A.c
+++ CodeForces/BuyAShovel732A.c
@@ -2,6 +2,24 @@
 
 int main()
 {
-    
+    int k,r;
+    scanf("%d%d",&k,&r);
+
+    int flag = 1;
+    for(int i = 1; i<10; i++)
+    {
+        int t = k*i;
+        if(t%10 == r || t%10 == 0)
+        {
+            flag=0;
+            printf("%d", i);
+            break;
+        }
+    }
+
+    if(flag == 1)
+    {
+        printf("10");
+    }
     return 0;
 }
--- CodeForces/SumOfRoundNumbers1352A.c
+++ CodeForces/SumOfRoundNumbers1352A.c
@@ -1,7 +1,56 @@
 #include<stdio.h>
 
+int power(int number, int power)
+{
+    int ans = 1;
+    while (power > 0)
+    {
+        ans = ans*number;
+        power--;
+    }
+
+    return ans;
+}
+
 int main()
 {
-    
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int num;  
+        scanf("%d", &num);
+
+        int nonZeroDigits = 0;
+        int copy = num;
+        
+        while(copy > 0)
+        {
+            int digit = copy%10;
+            
+            if(digit != 0)
+                nonZeroDigits++;
+            
+            copy /= 10;
+        }
+        printf("%d\n",nonZeroDigits);
+        copy = num;
+        int placeValue = 1;
+        
+        while (copy > 0)
+        {
+            int digit = copy%10;
+            
+            if(digit !=0)
+            {
+                int factor = power(10, placeValue);
+                printf("%d ", digit*factor/10);
+            }
+            placeValue++;
+            copy /= 10;
+        }
+        printf("\n");
+    }
     return 0;
 }
