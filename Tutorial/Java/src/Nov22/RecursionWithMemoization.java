1a1df419afc94407bd93c4ffca864d3fd5841352
Anku-0101 <53900735+Anku-0101@users.noreply.github.com>
2022-12-29 23:38:35 +0530

    Create RecursionWithMemoization.java

---
+++ Tutorial/Java/src/Nov22/RecursionWithMemoization.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Main{
+    
+    public static int[] arr = new int[100];
+    
+    public static void main(String[] args){
+        System.out.println("New Fib executing");
+        System.out.println(Fib(60));
+        System.out.println("New Fib executed");
+        
+        System.out.println("Old Fib executing");
+        System.out.println(FibOld(60));
+        System.out.println("Old Fib executed");
+    }
+    
+    public static int Fib(int n){
+        if(n < 2){
+            arr[n] = n;
+            return n;
+        }
+        
+        if(arr[n] != 0)
+            return arr[n];
+        
+        //arr[n] = Fib(n-1) + Fib(n-2);
+        //return arr[n]; 
+        return arr[n] = Fib(n-1) + Fib(n-2);
+    }
+    
+    public static int FibOld(int n){
+        if(n < 2)
+            return n;
+            
+        return FibOld(n-1) + FibOld(n-2);
+    }
+    
+    public static void Print(int n) {
+        if (n==1) {
+            System.out.println(n);
+            return;   
+        }
+        else{
+            Print(n-1);
+            System.out.println(n);
+        }
+    }
+
+}
