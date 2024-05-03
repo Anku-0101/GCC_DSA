f8c6c59626c4e4917da9fdb40a71dfae3bd773ae
Anku-0101 <53900735+Anku-0101@users.noreply.github.com>
2024-05-03 21:38:15 +0530

    Create Que4_Disc_Prob.py

---
+++ Tutorial/DS_Certification_Apr/Que4_Disc_Prob.py
@@ -0,0 +1,18 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# range of 1 to 10
+# for k in range(15):
+#    print(k)
+
+import math
+
+def binomial_prob(n,p,x):
+    return (math.comb(n,x))*(p**x)*((1-p)**(n-x))
+    
+##plug the values
+p = 0.95
+n = 20
+
+# p(x=18) + p(x=19) + p(x=20)
+prob_atleast_18 = sum(binomial_prob(n,p,x)for x in range(18,21))
+print(prob_atleast_18)
