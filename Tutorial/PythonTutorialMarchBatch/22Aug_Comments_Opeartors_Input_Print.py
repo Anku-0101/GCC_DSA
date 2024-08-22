cc083b1a57594c256191fe43f11fa8c55777d5f6
Anku-0101 <53900735+Anku-0101@users.noreply.github.com>
2024-08-22 22:55:55 +0530

    Create 22Aug_Comments_Opeartors_Input_Print.py

---
+++ Tutorial/PythonTutorialMarchBatch/22Aug_Comments_Opeartors_Input_Print.py
@@ -0,0 +1,21 @@
+name = "Raj"
+age = 20
+print("Name : "+ name, "age : ", age)
+print(f"Name: {name}, Age: {age}")
+print("Name : {}, Age: {}".format(name, age))
+
+print(type(name))
+print(type(age))
+'''
+num1 = input("Enter first number ") # a
+num2 = input("Enter second number ") # b
+sum = num1 + num2 # "a" + "b" = "ab"
+print("sum  = " + sum)
+'''
+'''
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter second number "))
+
+product = num1*num2
+print(product)
+'''
