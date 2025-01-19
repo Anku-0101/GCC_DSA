ac084d56f7448ce62ce3f875e03a39e282a7f5c7
Anku-0101 <53900735+Anku-0101@users.noreply.github.com>
2025-01-20 00:03:34 +0530

    Update TriangleType.sh

--- linux/TriangleType.sh
+++ linux/TriangleType.sh
@@ -4,11 +4,9 @@ read x
 read y
 read z
 
-if [ $x -eq $y ]; then
-    if [ $x -eq $z ]; then
-        echo "EQUILATERAL"
-    fi
-elif [ $x -eq $y || $y -eq $z || $z -eq $x ]; then
+if [[ $x -eq $y && $y -eq $z ]]; then
+    echo "EQUILATERAL"
+elif [[ $x -eq $y || $y -eq $z || $z -eq $x ]]; then
     echo "ISOSCELES"
 else 
     echo "SCALENE"
