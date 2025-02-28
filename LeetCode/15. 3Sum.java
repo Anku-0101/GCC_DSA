ad553a020e45f686970841e8aa1a89b3b41c7aa0
Anku-0101 <53900735+Anku-0101@users.noreply.github.com>
2025-02-28 20:41:27 +0530

    Create 15. 3Sum.java

---
+++ LeetCode/15. 3Sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> threeSum(int[] nums) {    
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1])continue;
+            int num1 = nums[i];
+            getSum(nums, -1*num1, i+1, n-1);
+        }
+        return ans;
+    }
+    
+    int[] getSum(int[] nums, int target, int start, int end){
+        while(start < end){
+            
+            int sum = nums[start] + nums[end];
+            if(sum == target){
+                ans.add(new ArrayList<>(Arrays.asList(-1*target, nums[start], nums[end])));
+                while(start < end && nums[start] == nums[start+1])start++;
+                while(start < end && nums[end-1] == nums[end])end--;
+            }
+            if(sum > target){
+                end--; 
+            }else{
+                start++;
+            }
+        }
+        return null;
+    }
+}
